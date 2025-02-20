7050c36db4b1a72950ecb7a8a08e4bb5470a97fa
Benjamin Gutman <benjamingutman@icloud.com>
2025-02-20 11:50:28 -0500

    Added Passenger files to question 4 and 6 folders

---
+++ Question 4/Passenger.cpp
@@ -0,0 +1,48 @@
+////Benjamin Gutman 40315265
+////Jiyong Jeon 40314593
+
+#include "Passenger.h"
+
+#include <iostream>
+
+using namespace std;
+
+// Default Constructor
+Passenger::Passenger() : id(""), firstName(""), lastName(""), address(""), email("") {}
+
+// Parameterized Constructor
+Passenger::Passenger(string id, string firstName, string lastName, string address, string email)
+    : id(id), firstName(firstName), lastName(lastName), address(address), email(email) {}
+
+// Copy Constructor
+Passenger::Passenger(const Passenger &other)
+    : id(other.id), firstName(other.firstName), lastName(other.lastName), address(other.address), email(other.email) {}
+
+// Destructor
+Passenger::~Passenger() {}
+
+// Accessor Methods
+string Passenger::getId() const { return id; }
+string Passenger::getFirstName() const { return firstName; }
+string Passenger::getLastName() const { return lastName; }
+string Passenger::getAddress() const { return address; }
+string Passenger::getEmail() const { return email; }
+
+// Mutator Methods
+void Passenger::setId(string id) { this->id = id; }
+void Passenger::setFirstName(string firstName) { this->firstName = firstName; }
+void Passenger::setLastName(string lastName) { this->lastName = lastName; }
+void Passenger::setAddress(string address) { this->address = address; }
+void Passenger::setEmail(string email) { this->email = email; }
+
+// Print Information
+void Passenger::printInfo() const {
+    cout << "Passenger ID: " << id << endl;
+    cout << "First Name: " << firstName << endl;
+    cout << "Last Name: " << lastName << endl;
+    cout << "Address: " << address << endl;
+    cout << "Email: " << email << endl;
+}
+
+
+
---
+++ Question 6/Passenger.h
@@ -0,0 +1,45 @@
+////Benjamin Gutman 40315265
+////Jiyong Jeon 40314593
+
+#ifndef PASSENGER_H
+#define PASSENGER_H
+
+#include <string>
+using namespace std;
+
+class Passenger {
+private:
+    string id;
+    string firstName;
+    string lastName;
+    string address;
+    string email;
+
+public:
+    // Constructors
+    Passenger();
+    Passenger(string id, string firstName, string lastName, string address, string email);
+    Passenger(const Passenger &other); // Copy Constructor
+
+    // Destructor
+    ~Passenger();
+
+    // Accessors
+    string getId() const;
+    string getFirstName() const;
+    string getLastName() const;
+    string getAddress() const;
+    string getEmail() const;
+
+    // Mutators
+    void setId(string id);
+    void setFirstName(string firstName);
+    void setLastName(string lastName);
+    void setAddress(string address);
+    void setEmail(string email);
+
+    // Print Function
+    void printInfo() const;
+};
+
+#endif /* PASSENGER_H */
